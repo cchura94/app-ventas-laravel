7bfdef8ef1c7e465105b4c8e0d7e05b21c08bbaf
cchura94 <cchura.cpc@gmail.com>
2020-09-16 20:09:18 -0400

    migraciones y models

---
+++ database/migrations/2020_09_16_193908_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombres", 30)->nullable();
+            $table->string("apellidos", 30);
+            $table->string("empresa", 30)->nullable();
+            $table->string("telefono", 30)->nullable();
+            $table->string("ci_nit", 15);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
